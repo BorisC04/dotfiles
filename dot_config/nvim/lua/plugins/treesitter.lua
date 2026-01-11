b88bdf5e60600295c112039c495aafb5d0918af3
BorisC04 <boriskayin@gmail.com>
2026-01-11 22:50:54 +0800

    nvim

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+	local config = require("nvim-treesitter")
+	config.install {
+	    "c",
+	    "lua",
+	    "vim",
+	    "vimdoc",
+	    "query",
+	    "markdown",
+	    "markdown_inline",
+	    "json",
+	    "bash",
+	}
+	config.setup({
+	    hightlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+	    },
+	    auto_install = false
+	})
+	vim.api.nvim_create_autocmd( 'FileType', {
+	    pattern = { '<fileType>'},
+	    callback = function() vim.treesitter.start()
+	    vim.wo[0][0].fooldexpr = 'v:lua.vim.treesitter.foldexpr()'
+	    vim.wo[0][0].foldmethod = 'expr'
+	    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	    end,
+	})
+    end
+}
---
+++ dot_vim/colors.vim
@@ -0,0 +1,11 @@
+set termguicolors
+
+
+let g:tokyonight_style = 'night'
+let g:tokyonight_enable_italic = 1
+let g:tokyonight_transparent_background = 1
+
+set laststatus=2
+let g:lightline = {'colorscheme' : 'tokyonight'}
+
+colorscheme tokyonight
