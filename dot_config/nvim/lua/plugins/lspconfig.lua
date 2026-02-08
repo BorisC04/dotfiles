fbd9498818a688917be10cbe18288192f2f69eed
BorisC04 <boriskayin@gmail.com>
2026-02-08 17:25:34 +0800

    added Mason Tool Installer

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -3,14 +3,23 @@ return {
     dependencies = {
 	'mason-org/mason.nvim',
 	'mason-org/mason-lspconfig.nvim',
+	'WhoIsSethDaniel/mason-tool-installer.nvim',
     },
     config = function()
 	require('mason').setup({})
-	require('mason-lspconfig').setup({
+	require('mason-lspconfig').setup({})
+	require('mason-tool-installer').setup({
 	    ensure_installed = {
 		'lua_ls',
 		'qmlls',
-	    }
+	    },
 	})
+
+	-- qmlls for quickshell needs to be setup this way to work
+	-- vim.lsp.config('qmlls', {
+	--     cmd = {'qmlls', '-E'},
+	--     filetypes = { 'qml' }
+	-- })
+	-- vim.lsp.enable('qmlls')
     end
 }
