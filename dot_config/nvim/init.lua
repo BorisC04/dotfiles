12e521d75ad45445d064845d5ad392498729ec40
BorisC04 <103830375+BorisC04@users.noreply.github.com>
2026-02-15 21:13:33 +0800

    Nvim - An entire directory can be loaded using requireDirectory()
    Using requireDirectory() in init.lua can load all file in the directory automatically

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,3 +2,16 @@ require('config.options')
 require('config.keybinds')
 require('config.lazy')
 require('config.lsp')
+
+local function requireDirectory(dir)
+    local cwd = vim.fn.getcwd() .. "/" .. dir
+    local files = vim.split(vim.fn.glob(cwd .. "/*"), '\n', {trimempty=true})
+
+    for _, item in pairs(files) do
+	local module = item:gsub('.*%/', '')
+	module = module:gsub('.lua', '')
+	require(dir .. '.' .. module)
+    end
+end
+
+requireDirectory("lsp")
