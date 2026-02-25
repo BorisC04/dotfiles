4346a7039e19b7a64e5ba8b4ee9a77ec4c1aaf2c
BorisC04 <boriskayin@gmail.com>
2026-02-25 19:00:02 +0800

    nvim softtabstop

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 4
-vim.g.netrw_bufsettings = "noma nomod nu nobl noweap ro"
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
