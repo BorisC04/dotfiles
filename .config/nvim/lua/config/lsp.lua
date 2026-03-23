0be0acb9fd5ed3cafcacaabcbdd59e7e244b60f4
BorisC04 <boriskayin@gmail.com>
2026-03-23 17:37:08 +0800

    removed useless code

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,5 +1,4 @@
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method('textDocument/implementation') then
