a1d55e341be515b40e01b0a2ec64beeb1f08513d
BorisC04 <boriskayin@gmail.com>
2026-05-20 18:46:40 +0800

    swithcing from hybrid-sleep to suspend-then-hibernate

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -3,7 +3,7 @@
 CHOSEN=$(echo -e "Sleep\nHibernate\nReboot\nShutdown" | wofi --conf="$HOME"/.config/wofi/powermenu_config)
 
 case "$CHOSEN" in
-    "Sleep") systemctl hybrid-sleep ;;
+    "Sleep") systemctl suspend-then-hibernate ;;
     "Hibernate") systemctl hibernate ;;
     "Reboot") reboot ;;
     "Shutdown") shutdown -h now ;;
