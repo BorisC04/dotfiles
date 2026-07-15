89cbe9e6ace0a25031846003a11cc19901db3d1b
BorisC04 <boriskayin@gmail.com>
2026-07-15 18:44:46 +0800

    using blink.cmp, no need for manual configuration

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       -- Optional: trigger autocompletion on EVERY keypress. May be slow!
       -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
       -- client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
+      -- vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
     end
   end,
 })
