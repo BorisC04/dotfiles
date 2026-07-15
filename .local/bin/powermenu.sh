f8ee3136e640d522a43bd6091cbe8f7002504006
BorisC04 <boriskayin@gmail.com>
2026-07-15 18:42:51 +0800

    changing hybrid sleep to suspend

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -3,7 +3,7 @@
 CHOSEN=$(echo -e "Sleep\nHibernate\nReboot\nShutdown" | wofi --conf="$HOME"/.config/wofi/powermenu_config)
 
 case "$CHOSEN" in
-    "Sleep") systemctl suspend-then-hibernate ;;
+    "Sleep") systemctl suspend ;;
     "Hibernate") systemctl hibernate ;;
     "Reboot") reboot ;;
     "Shutdown") shutdown -h now ;;
