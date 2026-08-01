8d70d4fd98ef72608dc215dbc15a139013f8e453
BorisC04 <boriskayin@gmail.com>
2026-08-01 17:46:29 +0800

    using wildcard to include subfiles in hyprland.lua

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,19 +1,3 @@
 require("monitors")
-require("hyprland.startup")
-require("hyprland.general")
-require("hyprland.misc")
-require("hyprland.animations")
-require("hyprland.layout")
-require("hyprland.input")
-require("hyprland.keybinds")
 require("workspaces")
-require("hyprland.smart_gaps")
-require("hyprland.rules")
-
-local extra = io.popen("ls " .. os.getenv("HOME") .. "/.config/hypr/hyprland/extra/*.lua")
-if extra then
-    for file in extra:lines() do
-        dofile(file)
-    end
-    extra:close()
-end
+require("./hyprland/*")
