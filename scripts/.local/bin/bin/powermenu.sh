23faca313033b6c9fe301ccdf28c3819d2d3fc9d
BorisC04 <boriskayin@gmail.com>
2026-08-16 18:23:17 +0800

    restructured folders to follow GNU stow convention

--- scripts/.local/bin/bin/powermenu.sh
+++ scripts/.local/bin/bin/powermenu.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
-CHOSEN=$(echo -e "Sleep\nHibernate\nReboot\nShutdown" | wofi --conf="$HOME"/.config/wofi/powermenu_config)
+CHOSEN=$(echo -e "Sleep\nHibernate\nReboot\nShutdown" | rofi -dmenu -theme ~/.config/rofi/powermenu.rasi -mesg "Power Menu" -u 1:4 -a 1)
 
 case "$CHOSEN" in
     "Sleep") systemctl suspend ;;
     "Hibernate") systemctl hibernate ;;
     "Reboot") reboot ;;
     "Shutdown") shutdown -h now ;;
+    *) exit 0 ;;
 esac
